dfe727070b9607bb272b0f16a1da7ad91f963c33
tormozz48 <tormozz48@gmail.com>
2015-07-26 23:13:38 +0300

    Refactor src/document.js module

--- src/document.js
+++ src/document.js
@@ -1,34 +1,53 @@
-/* jshint ignore:start */
 var cheerio = require('cheerio'),
     inherit = require('inherit'),
-    Url = require('url'),
+    Url = require('url');
 
-    Document = module.exports = inherit({
-        /**
-         * Constructor function
-         * @param {String} url - request url
-         * @param {HttpResponse} res - request object
-         * @private
-         */
-        __constructor: function (url, res) {
-            this.res = res;
-            this.url = url;
-        },
+module.exports = inherit({
+    _res: undefined,
+    _url: undefined,
+    _$: undefined,
 
-        /**
-         * Parses response body by cheerio
-         * @returns {*|any|Object|undefined}
-         */
-        get $() {
-            return this.$ = cheerio.load(this.res.body);
-        },
+    /**
+     * Constructor function
+     * @param {String} url - request url
+     * @param {HttpResponse} res - response object
+     * @private
+     */
+    __constructor: function (url, res) {
+        this._res = res;
+        this._url = url;
+        this._$ = cheerio.load(res['body']);
+    },
 
-        /**
-         * Resolves parsed link against document url
-         * @param {String} link - document link
-         */
-        resolve: function (link) {
-            return Url.resolve(this.url, link);
-        }
-    });
-/* jshint ignore:end */
+    /**
+     * Returns response object
+     * @returns {HttpResponse|HttpsResponse}
+     */
+    get res() {
+        return this._res;
+    },
+
+    /**
+     * Returns document url
+     * @returns {String}
+     */
+    get url() {
+        return this._url;
+    },
+
+    /**
+     * Parses response body parsed by cheerio
+     * @returns {*|any|Object|undefined}
+     */
+    get $() {
+        return this._$;
+    },
+
+    /**
+     * Resolves parsed link against document url
+     * @param {String} link - document link
+     */
+    resolve: function (link) {
+        return Url.resolve(this._url, link);
+    }
+});
