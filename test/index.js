589d9ca82cbb8a0f6361f35c1e40d02217e3803d
tormozz48 <tormozz48@gmail.com>
2015-08-01 17:35:32 +0300

    Add tests for statistic model module code

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 require('./src/model/broken.test.js');
 require('./src/model/document.test.js');
+require('./src/model/statistic.test.js');
 require('./src/based-option.test.js');
 require('./src/based-rule.test.js');
 require('./src/skip-rules.test.js');
---
+++ test/src/model/statistic.test.js
@@ -0,0 +1,55 @@
+var should = require('should'),
+    Broken = require('../../../src/model/broken'),
+    Statistic = require('../../../src/model/statistic');
+
+describe('statistic', function () {
+    var statistic;
+
+    beforeEach(function () {
+        statistic = new Statistic();
+    });
+
+    describe('__constructor', function () {
+        it ('should have zero _countExternal value after initialization', function () {
+            statistic._countExternal.should.equal(0);
+        });
+
+        it ('should have zero _countInternal value after initialization', function () {
+            statistic._countInternal.should.equal(0);
+        });
+
+        it ('should have empty broken links model after initialization', function () {
+            statistic._broken.getAll().should.have.length(0);
+        });
+    });
+
+    it('should increase internal count', function() {
+        statistic.increaseInternalCount();
+        statistic.getInternalCount().should.equal(1);
+    });
+
+    it('should increase external count', function() {
+        statistic.increaseExternalCount();
+        statistic.getExternalCount().should.equal(1);
+    });
+
+    it('should return valid internal count value', function () {
+        statistic.getInternalCount().should.equal(0);
+        statistic.increaseInternalCount();
+        statistic.getInternalCount().should.equal(1);
+    });
+
+    it('should return valid external count value', function () {
+        statistic.getExternalCount().should.equal(0);
+        statistic.increaseExternalCount();
+        statistic.getExternalCount().should.equal(1);
+    });
+
+    it('should return broken links model', function () {
+        statistic.getBroken().should.be.instanceOf(Broken);
+    });
+
+    it('should return valid result of getBrokenCount method', function () {
+        statistic.getBrokenCount().should.equal(0);
+    });
+});
