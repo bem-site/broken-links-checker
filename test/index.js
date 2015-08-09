2a30401076febd0996437a40eb651c795cbd6c87
tormozz48 <tormozz48@gmail.com>
2015-08-09 22:50:12 +0300

    Add missed changes in test/index.js file

--- test/index.js
+++ test/index.js
@@ -3,7 +3,6 @@ require('./src/model/document.test.js');
 require('./src/model/statistic.test.js');
 require('./src/util.test.js');
 require('./src/based-option.test.js');
-require('./src/based-rule.test.js');
 require('./src/acts/config.test.js');
 require('./src/reporters/base.test');
 require('./src/checker.test.js');
