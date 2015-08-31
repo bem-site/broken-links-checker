6f23415417f89f40f2d07c78206611bd063bf37a
tormozz48 <tormozz48@gmail.com>
2015-08-31 16:09:30 +0300

    #5 Update tests for LinkAnalyzer module

--- test/src/link-analyzer.test.js
+++ test/src/link-analyzer.test.js
@@ -1,4 +1,5 @@
-var Url = require('url'),
+var Util = require('util'),
+    Url = require('url'),
     should = require('should'),
     BasedOption = require('../../lib/based-option'),
     LinkAnalyzer = require('../../lib/link-analyzer');
@@ -20,160 +21,101 @@ describe('LinkAnalyzer', function () {
         });
     });
 
-    describe('_skipNonAcceptableProtocols', function () {
-        var linkAnalyzer;
-
-        beforeEach(function () {
-            linkAnalyzer = new LinkAnalyzer('http://my.site.com', new BasedOption());
-        });
-
-        it ('should return true if scheme of given url does not exist in list of acceptable acceptedSchemes', function () {
-            linkAnalyzer._options.setOption({}, 'acceptedSchemes', ['http:', 'https:']);
-            linkAnalyzer._skipNonAcceptableProtocols('mailto://my.host/url1').should.equal(true);
-        });
-
-        it ('should return false if scheme of given url exists in list of acceptable acceptedSchemes', function () {
-            linkAnalyzer._options.setOption({}, 'acceptedSchemes', ['http:', 'https:']);
-            linkAnalyzer._skipNonAcceptableProtocols('http://my.host/url1').should.equal(false);
-        });
-    });
-
-    describe('_skipExternalUrls', function () {
-        var linkAnalyzer;
-
-        beforeEach(function () {
-            linkAnalyzer = new LinkAnalyzer('http://my.site.com', new BasedOption());
-        });
-
-        it ('should return false if checkExternalUrls option is set to true', function () {
-            linkAnalyzer._options.setOption({}, 'checkExternalUrls', true);
-            linkAnalyzer._skipExternalUrls('http://outer.host:80/url1').should.equal(false);
-        });
-
-        it ('should return true if host of given url is different then host of initial url', function () {
-            linkAnalyzer._options.setOption({}, 'checkExternalUrls', false);
-            linkAnalyzer._skipExternalUrls('http://outer.host:80/url1').should.equal(true);
-        });
-
-        it ('should return false if host of given url is the same as host of initial url', function () {
-            linkAnalyzer._options.setOption({}, 'checkExternalUrls', false);
-            linkAnalyzer._skipExternalUrls('http://my.site.com/url2').should.equal(false);
-        });
-
-    });
-
-    describe('_skipExcludedUrls', function () {
-        var linkAnalyzer;
-
-        function assert(url, expected, options) {
-            linkAnalyzer._options.setOption({}, 'excludeLinkPatterns', options);
-            linkAnalyzer._skipExcludedUrls(url).should.be.equal(expected);
-        }
-
-        function assertStringPattern(url, pattern, expected) {
-            it ('should match ' + url + ' to ' + pattern, function () {
-                assert(url, expected, [pattern]);
-            });
-        }
-
-        beforeEach(function () {
-            linkAnalyzer = new LinkAnalyzer('http://my.site.com', new BasedOption());
-        });
-
-        it ('should return false in case of empty "excludeLinkPatterns" array', function () {
-            assert('http://my.site.com/url1', false, []);
-        });
-
-        describe('regular expressions', function () {
-            it ('should return false if any of excluded patterns does not match given url', function () {
-                assert('http://my.site.com/url1', false, [/\/foo1/i, /\/foo2/i]);
-            });
-
-            it ('should return true if any excluded patterns matches on given url', function () {
-                assert('http://my.site.com/foo1', true, [/\/foo1/i, /\/foo2/i]);
-            });
-        });
-
-        describe('string patterns', function () {
-            assertStringPattern('http://my.site.com/foo/bar', 'http://my.site.com/foo/bar', true);
-            assertStringPattern('http://my.site.com/foo/bar', 'http://my.site.com/foo/*', true);
-            assertStringPattern('http://my.site.com/foo/bar', 'http://my.site.com/*/bar', true);
-            assertStringPattern('http://my.site.com/foo/bar', 'http://my.site.com/*', true);
-            assertStringPattern('http://my.site.com/foo/bar', '*/foo/bar', true);
-
-            assertStringPattern('http://my.site.com/foo/bar#anchor', 'http://my.site.com/foo/bar#anchor', true);
-            assertStringPattern('http://my.site.com/foo/bar#anchor', 'http://my.site.com/foo/*#anchor', true);
-            assertStringPattern('http://my.site.com/foo/bar#anchor', 'http://my.site.com/*/bar#anchor', true);
-
-            assertStringPattern('http://my.site.com/foo/bar', '/foo/bar', false);
-        });
-    });
-
-    describe('_skipOnMode', function () {
-        var linkAnalyzer;
-
-        beforeEach(function () {
-            linkAnalyzer = new LinkAnalyzer('http://my.site.com', new BasedOption());
-        });
-
-        it('should return false for mode "page" and direct links of target page', function () {
-           linkAnalyzer._options.setOption({}, 'mode', 'page');
-           linkAnalyzer._skipOnMode('http://my.site.com/foo1', 'http://my.site.com').should.be.equal(false);
-        });
-
-        it('should return true for mode "page" and non-direct links of target page', function () {
-           linkAnalyzer._options.setOption({}, 'mode', 'page');
-           linkAnalyzer._skipOnMode('http://my.site.com/foo2', 'http://my.site.com/foo1').should.be.equal(true);
-        });
-
-        it('should return false for mode "section" and sub-links of initial link', function() {
-           linkAnalyzer = new LinkAnalyzer('http://my.site.com/foo1', new BasedOption());
-           linkAnalyzer._options.setOption({}, 'mode', 'section');
-           linkAnalyzer._skipOnMode('http://my.site.com/foo1/foo2', 'http://my.site.com/foo1').should.be.equal(false);
-        });
-
-        it('should return false for mode "section" and sibling of initial link', function() {
-           linkAnalyzer = new LinkAnalyzer('http://my.site.com/foo1', new BasedOption());
-           linkAnalyzer._options.setOption({}, 'mode', 'section');
-           linkAnalyzer._skipOnMode('http://my.site.com/foo21', 'http://my.site.com/foo1').should.be.equal(false);
-        });
-
-        it('should return false for mode "section" and parent of initial link', function() {
-           linkAnalyzer = new LinkAnalyzer('http://my.site.com/foo1', new BasedOption());
-           linkAnalyzer._options.setOption({}, 'mode', 'section');
-           linkAnalyzer._skipOnMode('http://my.site.com', 'http://my.site.com/foo1').should.be.equal(false);
-        });
-
-        it('should return true for mode "section" and page outside of section', function() {
-           linkAnalyzer = new LinkAnalyzer('http://my.site.com/foo2', new BasedOption());
-           linkAnalyzer._options.setOption({}, 'mode', 'section');
-           linkAnalyzer._skipOnMode('http://my.site.com/foo3', 'http://my.site.com/foo1').should.be.equal(true);
-        });
-    });
-
     describe('isNeedToSkipUrl', function () {
-        var linkAnalyzer;
+        function assert(url, expected, options, baseUrl, hostUrl) {
+            var testName = Object.keys(options).reduce(function (prev, item) {
+                    return prev + Util.format(' and %s = %s', item, options[item ]);
+                }, Util.format('"isNeedToSkipUrl" should return "%s" for url: "%s"', expected, url));
 
-        beforeEach(function () {
-            linkAnalyzer = new LinkAnalyzer('http://my.site.com', new BasedOption());
-            linkAnalyzer._options.setOption({}, 'acceptedSchemes', ['http:', 'https:']);
-            linkAnalyzer._options.setOption({}, 'checkExternalUrls', false);
-            linkAnalyzer._options.setOption({}, 'excludeLinkPatterns', [/\/foo1/i, /\/foo2/i]);
+            var hostUrl = hostUrl || 'http://my.site.com',
+                analyzer = new LinkAnalyzer(hostUrl, new BasedOption());
+
+            analyzer._options.setOption({}, 'acceptedSchemes', []);
+            analyzer._options.setOption({}, 'excludeLinkPatterns', []);
+
+            Object.keys(options).forEach(function(key) {
+                analyzer._options.setOption({}, key, options[key]);
+            });
+
+            return it(testName, function () {
+                analyzer.isNeedToSkipUrl(url, baseUrl || hostUrl).should.be.equal(expected);
+            });
+        }
+
+        describe('protocols criteria', function () {
+            var options = { acceptedSchemes: ['http:', 'https:'] };
+
+            assert('mailto://my.site.com/url1', true, options);
+            assert('https://my.site.com/url1', false, options);
+            assert('http://my.site.com/url1', false, options);
         });
 
-        [
-            { url: 'mailto://my.site.com/url1', result: true },
-            { url: 'http://google.com', result: true },
-            { url: 'http://my.site.com/url1', result: false },
-            { url: 'http://my.site.com/foo1', result: true },
-            { url: 'http://my.site.com/url1/foo2', result: true },
-            { url: 'http://my.site.com/foo1/foo2', result: true },
-            { url: 'https://my.site.com/url2/', result: false },
-            { url: 'mailto://my.site.com/foo2', result: true },
-        ].forEach(function (item) {
-            it ('"isNeedToSkipUrl" should return "'+ item.result + '" for url: "' + item.url + '"', function () {
-                linkAnalyzer.isNeedToSkipUrl(item.url, 'http://my.site.com').should.be.equal(item.result);
-            });
+        describe('external criteria', function () {
+            assert('http://outer.host:80/url1', false, { checkExternalUrls: true, acceptedSchemes: ['http:'] });
+            assert('http://outer.host:80/url1', true, { checkExternalUrls: false, acceptedSchemes: ['http:'] });
+            assert('http://my.site.com/url2', false, { checkExternalUrls: false, acceptedSchemes: ['http:'] });
+        });
+
+        describe('excluded urls criteria', function () {
+            function getOptions(patterns) {
+                return {
+                    acceptedSchemes: ['http:'],
+                    excludeLinkPatterns: patterns
+                };
+            }
+
+            assert('http://my.site.com/url1', false, getOptions([/\/foo1/i, /\/foo2/i]));
+            assert('http://my.site.com/foo1', true, getOptions([/\/foo1/i]));
+
+            assert('http://my.site.com/foo/bar', true, getOptions(['http://my.site.com/foo/bar']));
+            assert('http://my.site.com/foo/bar', true, getOptions(['http://my.site.com/foo/*']));
+            assert('http://my.site.com/foo/bar', true, getOptions(['http://my.site.com/*/bar']));
+            assert('http://my.site.com/foo/bar', true, getOptions(['http://my.site.com/*']));
+            assert('http://my.site.com/foo/bar', true, getOptions(['*/foo/bar']));
+
+            assert('http://my.site.com/foo/bar#a', true, getOptions(['http://my.site.com/foo/bar#a']));
+            assert('http://my.site.com/foo/bar#a', true, getOptions(['http://my.site.com/foo/*#a']));
+            assert('http://my.site.com/foo/bar#a', true, getOptions(['http://my.site.com/*/bar#a']));
+
+            assert('http://my.site.com/foo/bar', false, getOptions(['/foo/bar']));
+        });
+
+        describe('mode criteria', function () {
+            function getOptions(mode) {
+                return {
+                    acceptedSchemes: ['http:'],
+                    mode: mode
+                };
+            }
+
+            assert('http://my.site.com/foo1', false, getOptions('page'));
+            assert('http://my.site.com/foo2', true, getOptions('page'), 'http://my.site.com/foo1');
+
+            assert('http://my.site.com/foo1/foo2', false,
+                getOptions('section'), 'http://my.site.com/foo1', 'http://my.site.com/foo1');
+            assert('http://my.site.com/foo21', false,
+                getOptions('section'), 'http://my.site.com/foo1', 'http://my.site.com/foo1');
+            assert('http://my.site.com', false,
+                getOptions('section'), 'http://my.site.com/foo1', 'http://my.site.com/foo1');
+            assert('http://my.site.com/foo3', true,
+                getOptions('section'), 'http://my.site.com/foo1', 'http://my.site.com/foo2');
+        });
+
+        describe('complex', function () {
+            var options1 = {
+                acceptedSchemes: ['http:', 'https:'],
+                checkExternalUrls: false,
+                excludeLinkPatterns: [/\/foo1/i, /\/foo2/i]
+            };
+
+            assert('mailto://my.site.com/url1', true, options1);
+            assert('http://google.com', true, options1);
+            assert('http://my.site.com/url1', false, options1);
+            assert('http://my.site.com/foo1', true, options1);
+            assert('http://my.site.com/url1/foo2', true, options1);
+            assert('http://my.site.com/foo1/foo2', true, options1);
+            assert('https://my.site.com/url2/', false, options1);
+            assert('mailto://my.site.com/foo2', true, options1);
         });
     });
-})
+});
